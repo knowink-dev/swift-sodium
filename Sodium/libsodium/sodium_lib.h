2569d5489a737df2bb389e458a75b80d940442b5
Mark Schisler <mark.schisler@knowink.com>
2019-04-12 23:31:18 -0500

    Correcting compiler includes

--- Sodium/libsodium/sodium_lib.h
+++ Sodium/libsodium/sodium_lib.h
@@ -31,7 +31,6 @@
 #include "crypto_onetimeauth_poly1305.h"
 #include "crypto_pwhash.h"
 #include "crypto_pwhash_argon2i.h"
-#include "crypto_pwhash_scryptsalsa208sha256.h"
 #include "crypto_scalarmult.h"
 #include "crypto_scalarmult_curve25519.h"
 #include "crypto_secretbox.h"
