2348676af8ed18c0c942561423075378934c16d8
Chris Brown <christopher.brown@knowink.com>
2021-03-10 10:51:48 -0500

    Removed conditionals from package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,6 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let clibsodiumTarget: Target
-#if os(OSX) || os(macOS) || os(tvOS) || os(watchOS) || os(iOS)
-    clibsodiumTarget = .binaryTarget(
-        name: "Clibsodium",
-        path: "Clibsodium.xcframework")
-#else
-    clibsodiumTarget = .systemLibrary(
-        name: "Clibsodium",
-        path: "Clibsodium",
-        pkgConfig: "libsodium",
-        providers: [
-            .apt(["libsodium-dev"]),
-            .brew(["libsodium"]),
-            // Waiting for bug to be fixed: https://bugs.swift.org/browse/SR-14038
-            // .yum(["libsodium-devel"])
-        ])
-#endif
-
 let package = Package(
     name: "Sodium",
     products: [
@@ -30,7 +12,9 @@ let package = Package(
             targets: ["Sodium"]),
     ],
     targets: [
-        clibsodiumTarget,
+        .binaryTarget(
+            name: "Clibsodium",
+            path: "Clibsodium.xcframework"),
         .target(
             name: "Sodium",
             dependencies: ["Clibsodium"],
